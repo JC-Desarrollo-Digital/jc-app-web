3db8f36e0c35b5d18319a00d2ad5a8caff0a0c86
Perla Ramirez <perlaramireziciQ@gmail.com>
2026-07-23 12:33:40 -0600

    Agrega estilos y estructura del header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,34 @@
-// Header.tsx
-export default function Header() {
-  return <h1>JC Desarrollo Digital</h1>;
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+
+      <div className="brand">
+
+        <div className="logo-box">
+          JC
+        </div>
+
+        <div className="brand-name">
+          <h1>JC Desarrollo Digital</h1>
+          <p>Soluciones Tecnológicas</p>
+        </div>
+
+      </div>
+
+
+      <nav className="nav">
+        <ul>
+          <li><a href="/">Inicio</a></li>
+          <li><a href="/servicios">Servicios</a></li>
+          <li><a href="/proyectos">Proyectos</a></li>
+          <li><a href="/contacto">Contacto</a></li>
+        </ul>
+      </nav>
+
+    </header>
+  );
 }
+
+export default Header;
