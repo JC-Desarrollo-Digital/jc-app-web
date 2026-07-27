febdec3925e20fd0dbd83b28222abd22e4484493
Tu Nombre <juan.calleross@hotmail.com>
2026-07-27 10:45:14 -0600

    chore:Actualizacion de app.tsx con los componentes nuevos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Services />
+      <About />
+      <Contact />
       <Footer />
     </>
   );
